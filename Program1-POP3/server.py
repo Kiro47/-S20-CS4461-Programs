a122dc311abaa7253c7d0dda228bbaf7deffe5aa
JC Helm <jchelm@mtu.edu>
2020-02-17 01:15:36 -0500

    Add basic server side socket listener

--- Program1-POP3/server.py
+++ Program1-POP3/server.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import os
 import re
+import socket
 
 class Message(object):
 
@@ -139,6 +140,69 @@ class Mail_Repo(object):
             self.logging.error("Attempting to save empty Message object... Ignoring")
         return
 
+class Server(object):
+
+    """
+    Server loop instance
+    """
+
+    mail_repo = None
+
+    def __init__(self, mail_repo: Mail_Repo, port: int):
+        """
+        Server instnace
+
+        :mail_repo: Mail repo object
+        :port: Port to start on
+        """
+        # Set logging info
+        self.logging = logging.getLogger(self.__class__.__name__)
+        logging.debug("Init for Server Listener")
+        if mail_repo:
+            self.mail_repo = mail_repo
+        else:
+            logging.error("Mail Repo instance does not exit!")
+            return
+        self.server_runner(port)
+
+    def client_listener(self, sock, hostname: str):
+        """
+        Client Listener/interactions
+
+        :sock: Client Socket
+        :hostname: Hostname of client
+        """
+        pass
+
+    def server_runner(self, port: int):
+        """
+        Main Server runner instance
+        """
+        sockets = list()
+        # new socket setup
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((socket.gethostname(), port))
+        sock.listen(10)
+
+        try:
+            while True:
+                client_sock, client_addr = sock.accept()
+                logging.info("Accepting connection from [{}]".format(client_addr))
+                sockets.append(client_sock)
+                client_thread = threading.Thread(target=self.client_listener, args=(client_sock, client_addr))
+                client_thread.start()
+        except KeyboardInterrupt as keeb_exception:
+            self.logging.info("Shutting down server")
+            return
+        except Exception as exception:
+            self.logging.exception("Exception during main listener loop")
+        # Close all client sockets and main socket
+        for active_socket in sockets:
+            if active_socket:
+                active_socket.close()
+        sock.close()
+        return
+
 
 def check_debug_mode(debug):
     """
@@ -185,6 +249,7 @@ def main():
     mail_repo = Mail_Repo(directory)
     # Start Server
     logging.info("Starting Server on port: {}".format(args.Port))
+    Server(mail_repo, args.Port)
 
 if __name__ == "__main__":
     main()
